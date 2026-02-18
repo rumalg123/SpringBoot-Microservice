6a724204641b463517fefbb0e01e2098ed60fad3
Rumal Gunawardana <rumalg123@gmail.com>
2026-02-19 00:45:56 +0530

    bean issue fix

--- Services/admin-service/src/main/java/com/rumal/admin_service/config/HttpClientConfig.java
+++ Services/admin-service/src/main/java/com/rumal/admin_service/config/HttpClientConfig.java
@@ -8,11 +8,18 @@ import org.apache.hc.core5.util.Timeout;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class HttpClientConfig {
+
+    @Bean
+    @Primary
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
 
     @Bean
     @LoadBalanced
