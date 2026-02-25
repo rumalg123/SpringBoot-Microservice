497d4c498523a2b209f4692149fb245e0ad897df
Rumal Gunawardana <rumalg123@gmail.com>
2026-02-25 17:40:52 +0530

    review service

---
+++ Services/personalization-service/src/test/java/com/rumal/personalization_service/PersonalizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumal.personalization_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
