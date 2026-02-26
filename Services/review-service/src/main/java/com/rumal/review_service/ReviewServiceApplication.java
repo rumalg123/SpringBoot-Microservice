e6eb256bfb548039996cfae6655533059b95fb39
Rumal Gunawardana <rumalg123@gmail.com>
2026-02-26 19:00:52 +0530

    Refactor

--- Services/review-service/src/main/java/com/rumal/review_service/ReviewServiceApplication.java
+++ Services/review-service/src/main/java/com/rumal/review_service/ReviewServiceApplication.java
@@ -3,9 +3,11 @@ package com.rumal.review_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
